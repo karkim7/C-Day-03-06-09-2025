6601b12d266ee0fc89d721065f344b737e153378
Mahesh Karki <karkim@carolinau.edu>
2025-06-09 21:41:19 +0000

    ok

---
+++ Scruitiny/Program.cs
@@ -0,0 +1,24 @@
+﻿/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System;
+class HelloWorld 
+{
+  static void Main() 
+  {
+      
+      
+int[] numbers = {10, 28, 30};
+int sum = 0; 
+foreach (int num in numbers)
+{
+    sum +=num;
+}
+    Console.WriteLine("Sum: " + sum);
+  }
+}
